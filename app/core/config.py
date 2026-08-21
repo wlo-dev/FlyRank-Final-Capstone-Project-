be13b8bc1ac47f2f2ee528022479abc48d496c85
Willouby Yagontha  Chaguluka <chagulukawillouby0@gmail.com>
2026-08-21 17:38:44 +0200

    save

--- app/core/config.py
+++ app/core/config.py
@@ -2,5 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     ollama_base_url: str
-    ollama_visison_model: str
+    ollama_vision_model: str
     ollam_embed_model: str
+    
+    
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
+    postgres_user: str
+    postgres_password: str
