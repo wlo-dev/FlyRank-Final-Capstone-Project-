7ec7085104189141fa6ae28f8e4a9db3eff26d7a
Willouby Yagontha  Chaguluka <chagulukawillouby0@gmail.com>
2026-08-24 10:28:02 +0200

    yield

--- app/db/session.py
+++ app/db/session.py
@@ -6,3 +6,10 @@ from app.core.config import settings
 engine = create_engine(settings.database_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
