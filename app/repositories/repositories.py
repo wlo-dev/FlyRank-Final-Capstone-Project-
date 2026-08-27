5e32e5f6da5846cb02f75a2274a02010c78f69ca
Willouby Yagontha  Chaguluka <chagulukawillouby0@gmail.com>
2026-08-27 19:19:49 +0200

    update

--- app/repositories/repositories.py
+++ app/repositories/repositories.py
@@ -6,3 +6,13 @@ from app.models.image import Image
 class ImageRepository:
     def __init__(self, db: Session) :
         self.db = db
+        
+    def create(self,filename: str, filepath: str) -> Image:
+        image = Image(filename=filename, filepath=filepath)
+        self.db.add(image)
+        self.db.commit()
+        self.db.refresh(image)
+        return image
+    
+    def get_by_id(self, image_id: int) -> Image | None:
+        return self.db.get(Image, image_id)
