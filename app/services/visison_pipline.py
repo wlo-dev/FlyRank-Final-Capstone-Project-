c21ba4660344f742e9c1d25d378ed2ef13185bef
Willouby Yagontha  Chaguluka <chagulukawillouby0@gmail.com>
2026-08-28 18:21:52 +0200

    update

--- app/services/visison_pipline.py
+++ app/services/visison_pipline.py
@@ -6,3 +6,7 @@ import ollama
 from app.core.config import settings
 from app.db.session import SessionLocal
 from app.repositories.image_repository import ImageRepository
+
+def get_image_files() -> list[Path]:
+    corpus_dir = Path(settings.image_corpus-dir)
+    return list(corpus_dir.glob("*.jpg"))
