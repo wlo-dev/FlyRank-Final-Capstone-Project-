2221c16735ec6395aec15387f4820cb786b64e48
Willouby Yagontha  Chaguluka <chagulukawillouby0@gmail.com>
2026-09-01 19:24:20 +0200

    execute

--- app/services/matching_service.py
+++ app/services/matching_service.py
@@ -4,3 +4,12 @@ from sqlalchemy.orm import Session
 
 from app.models.image import Image
 from app.services.vision_pipeline import generate_embeddings
+
+
+def find_best_matches(db: Session, blog_text: str, top_k: int = 3):
+    query_embedding = generate_embedding(blog_text)
+    
+    
+    result = db.execute(
+        
+    )
