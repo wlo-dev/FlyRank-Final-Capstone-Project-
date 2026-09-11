dcc1a45d9923368da5ae578bac6e07ef5e5f0b7b
Willouby Yagontha  Chaguluka <chagulukawillouby0@gmail.com>
2026-09-11 12:48:45 +0200

    update

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 from app.api.routes import router
 
@@ -11,5 +12,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+app.mount("/static-images", StaticFiles(directory="data/images"), name="static-images")
 
 app.include_router(router)
